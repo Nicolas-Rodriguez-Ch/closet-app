b0c1c3b7fb9ce8f495a9005c020c06b8618c73c7
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-03-31 12:01:30 -0500

    adds testing setup files

---
+++ jest.config.mjs
@@ -0,0 +1,22 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
+
+export default createJestConfig(config);
---
+++ jest.setup.js
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+
+jest.mock('@/database/db', () => ({
+  __esModule: true,
+  default: jest.fn().mockImplementation(() => {
+    console.log('Mocked MongoDB connection');
+    return Promise.resolve();
+  }),
+}));
+
+jest.mock('next/navigation', () => ({
+  usePathname: jest.fn().mockReturnValue('/'),
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+  }),
+}));
