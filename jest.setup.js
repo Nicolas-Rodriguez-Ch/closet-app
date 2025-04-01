36067d28211d1abaa3e3020561d65889b160d5c3
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-03-31 20:04:58 -0500

    adds SEO to ENV

--- jest.setup.js
+++ jest.setup.js
@@ -1,13 +1,5 @@
 import '@testing-library/jest-dom';
 
-// jest.mock('@/database/db', () => ({
-//   __esModule: true,
-//   default: jest.fn().mockImplementation(() => {
-//     console.log('Mocked MongoDB connection');
-//     return Promise.resolve();
-//   }),
-// }));
-
 jest.mock('next/navigation', () => ({
   usePathname: jest.fn().mockReturnValue('/'),
   useRouter: jest.fn().mockReturnValue({
