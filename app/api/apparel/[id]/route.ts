cadae6380f7f2bb619a2d1e7ae755687bba2ae2e
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-04-01 17:58:34 -0500

    finishes apparel CRUD end points

---
+++ app/api/apparel/[id]/route.ts
@@ -0,0 +1,86 @@
+import Apparel from '@/database/models/apparel';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(request: NextRequest, { params }: Params) {
+  try {
+    const { id } = await params;
+
+    const apparel = await Apparel.findOne({ id });
+
+    if (!apparel) {
+      return NextResponse.json(
+        { message: `Apparel with ID ${id} not fouind` },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(apparel, { status: 200 });
+  } catch (error) {
+    console.error(`Error fetching apparel with ID ${params.id}:`, error);
+    return NextResponse.json(
+      {
+        message: 'Failed to fetch Apparel item',
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: Params) {
+  try {
+    const { id } = await params;
+    const body = await request.json();
+
+    const updatedApparel = await Apparel.findOneAndUpdate(
+      { id },
+      { $set: body },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedApparel) {
+      return NextResponse.json(
+        { message: `Apparel with ID ${id} not found` },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(updatedApparel, { status: 200 });
+  } catch (error) {
+    console.error(`Error updating apparel with ID ${params.id}:`, error);
+    return NextResponse.json(
+      { message: 'Failed to update Apparel item' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest, { params }: Params) {
+  try {
+    const { id } = await params;
+
+    const deletedApparel = await Apparel.findOneAndDelete({ id });
+
+    if (!deletedApparel) {
+      return NextResponse.json(
+        { message: `Apparel with ID ${id} not found` },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      { message: `Apparel with ID ${id} deleted successfully` },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(`Error deleting apparel with ID ${params.id}:`, error);
+    return NextResponse.json(
+      { message: 'Failed to delete apparel item' },
+      { status: 500 }
+    );
+  }
+}
