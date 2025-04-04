bbed1b9c947e9be795f969d805c7f2a105d13440
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-04-04 11:01:13 -0500

    minor style changes

--- app/components/NavBar/navBar.tsx
+++ app/components/NavBar/navBar.tsx
@@ -7,10 +7,10 @@ import React from 'react';
 const NavBar = () => {
   const pathname = usePathname();
   return (
-    <header className='bg-palette-5 flex flex-row gap-5 p-5 md:gap-8 md:p-8 items-center justify-around'>
+    <header className='bg-palette-5 flex flex-row gap-1 p-5 md:gap-8 md:p-8 items-center justify-around'>
       <Link
         href='/'
-        className={`text-palette-3 font-bold md:text-xl px-3 py-1 relative z-10 transition-colors duration-300 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-full ${
+        className={`text-palette-3 text-center font-bold md:text-xl px-3 py-1 relative z-10 transition-colors duration-300 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-full ${
           pathname === '/' ? 'before:w-full' : 'before:w-0'
         } before:bg-palette-2 before:transition-all before:rounded-lg hover:text-palette-3 hover:before:w-full`}
       >
@@ -18,7 +18,7 @@ const NavBar = () => {
       </Link>
       <Link
         href='/outfits'
-        className={`text-palette-3 font-bold md:text-xl px-3 py-1 relative z-10 transition-colors duration-300 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-full ${
+        className={`text-palette-3 text-center font-bold md:text-xl px-3 py-1 relative z-10 transition-colors duration-300 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-full ${
           pathname === '/outfits' ? 'before:w-full' : 'before:w-0'
         } before:bg-palette-2 before:transition-all before:rounded-lg hover:text-palette-3 hover:before:w-full`}
       >
@@ -26,7 +26,7 @@ const NavBar = () => {
       </Link>
       <Link
         href='/upload'
-        className={`text-palette-3 font-bold md:text-xl px-3 py-1 relative z-10 transition-colors duration-300 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-full ${
+        className={`text-palette-3 text-center font-bold md:text-xl px-3 py-1 relative z-10 transition-colors duration-300 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:-z-10 before:h-full ${
           pathname === '/upload' ? 'before:w-full' : 'before:w-0'
         } before:bg-palette-2 before:transition-all before:rounded-lg hover:text-palette-3 hover:before:w-full`}
       >
