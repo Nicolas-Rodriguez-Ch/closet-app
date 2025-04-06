ad1350fcd2bb862706921b3162adffbd2ba8caf4
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-04-06 17:27:57 -0500

    adds responsive styles

--- app/components/CarouselComponent/CarouselComponent.tsx
+++ app/components/CarouselComponent/CarouselComponent.tsx
@@ -13,36 +13,36 @@ const CarouselComponent = ({ item, category }: CarouselComponentProps) => {
   };
 
   return (
-    <div className='w-full max-w-md mx-auto px-4 py-6 bg-palette-3'>
-      <section className='flex flex-col items-center space-y-4 bg-white rounded-lg shadow-md p-6'>
-        <h1 className='text-2xl font-bold text-palette-2 uppercase tracking-wider mb-2'>
-          {category}
-        </h1>
+    <div className='w-full px-4 py-6 bg-palette-3'>
+    <section className='w-full max-w-[500px] mx-auto flex flex-col items-center space-y-4 bg-white rounded-lg shadow-md p-4'>
+      <h1 className='text-lg font-bold text-palette-2 uppercase tracking-wider'>
+        {category}
+      </h1>
 
-        <div className='flex items-center justify-between w-full space-x-4'>
-          <button
-            onClick={showPreviousItem}
-            className='bg-palette-1 text-white w-1/4 p-1 rounded-4xl hover:bg-palette-5 transition-colors duration-300 ease-in-out'
-          >
-            Previous
-          </button>
+      <div className='flex items-center justify-between w-full gap-4'>
+        <button
+          onClick={showPreviousItem}
+          className='flex-1 bg-palette-1 text-white py-2 px-4 rounded-full hover:bg-palette-5 transition-colors'
+        >
+          Previous
+        </button>
 
-          <button
-            onClick={showNextItem}
-            className='bg-palette-1 text-white w-1/4 p-1 rounded-4xl hover:bg-palette-5 transition-colors duration-300 ease-in-out'
-          >
-            Next
-          </button>
-        </div>
+        <button
+          onClick={showNextItem}
+          className='flex-1 bg-palette-1 text-white py-2 px-4 rounded-full hover:bg-palette-5 transition-colors'
+        >
+          Next
+        </button>
+      </div>
 
-        <div className='w-full max-w-xs aspect-square flex items-center justify-center'>
-          <ApparelSlide
-            imgSrc={item[activeIndex].pictureURL}
-            title={item[activeIndex].title}
-          />
-        </div>
-      </section>
-    </div>
+      <div className='w-full h-[400px]'>
+        <ApparelSlide
+          imgSrc={item[activeIndex].pictureURL}
+          title={item[activeIndex].title}
+        />
+      </div>
+    </section>
+  </div>
   );
 };
 
