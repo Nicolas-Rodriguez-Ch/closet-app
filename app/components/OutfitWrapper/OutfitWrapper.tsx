be29bb583c0cab0588cba840aaa20af2343fcb77
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-04-07 15:37:49 -0500

    adds responsive styles

--- app/components/OutfitWrapper/OutfitWrapper.tsx
+++ app/components/OutfitWrapper/OutfitWrapper.tsx
@@ -14,19 +14,25 @@ const OutfitWrapper = () => {
   useEffect(() => {
     dispatch(fetchAllOutfits());
   }, [dispatch]);
-  
+
   return (
-    <>
+    <div className='bg-palette-4/80 min-h-screen w-full py-3 sm:py-6'>
       {(status === 'idle' || status === 'loading') && <LoadingComponent />}
       {status === 'failed' && <ErrorComponent />}
       {status === 'succeeded' && (
-        <>
-          {items.map((item) => (
-            <OutfitItem key={item.id} item={item} />
-          ))}
-        </>
+        <div className='container mx-auto px-3 sm:px-6'>
+          <div
+            className={`grid grid-cols-1 sm:grid-cols-2 ${
+              items.length <= 3 ? 'lg:grid-cols-3' : 'lg:grid-cols-4'
+            } gap-3 sm:gap-5`}
+          >
+            {items.map((item) => (
+              <OutfitItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
       )}
-    </>
+    </div>
   );
 };
 
